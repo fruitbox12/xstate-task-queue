0ac64cf5cd79d6259a6689be1b17030b2a262969
fruitbox12 <dylanwong007@gmail.com>
2023-11-30 03:25:38 -0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body><iframe
+        className="responsive-iframe"
+        src="https://mesh-xi.vercel.app/"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: "100%",
+          width: "100%",
+
+          border: "none",
+          zIndex: -1,
+        }}
+      ></iframe> 
         <Main />
         <NextScript />
       </body>
