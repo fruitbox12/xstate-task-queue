4aacd9d9218534d8f0d8febe182cadc2e6cb0ede
fruitbox12 <dylanwong007@gmail.com>
2023-11-30 03:29:33 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -219,7 +219,20 @@ export default function Home() {
               ></path>
             </svg>
           </a>
-        </Flex>
+        </Flex><iframe
+        className="responsive-iframe"
+        src="https://mesh-xi.vercel.app/"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: "100%",
+          width: "100%",
+
+          border: "none",
+          zIndex: -1,
+        }}
+      ></iframe> 
       </main>
     </>
   );
