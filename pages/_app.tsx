019dc000c749623fcd4c402193f1c14c094f46a6
fruitbox12 <dylanwong007@gmail.com>
2023-11-30 03:27:12 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Component {...pageProps} /><iframe
+        className="responsive-iframe"
+        src="https://mesh-xi.vercel.app/"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: "100%",
+          width: "100%",
+
+          border: "none",
+          zIndex: -1,
+        }}
+      ></iframe> 
     </ChakraProvider>
   );
 }
